5971e064599df3b2acfe33567a161bf575b25ab0
Jobair Al Sarkar <jobairalsarkar1@gmail.com>
2025-08-14 04:35:41 +0600

    User Extended with Role, Data saved in database, Middleware redirects based on role.

--- auth.ts
+++ auth.ts
@@ -1,10 +1,28 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthConfig, User } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 import { prisma } from "./lib/prisma"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+import { JWT } from "next-auth/jwt"
+
+export const config = {
   adapter: PrismaAdapter(prisma),
   providers: [GitHub, Google],
-})
+  session: { strategy: 'jwt' },
+  callbacks: {
+    async jwt({ token, user }: {token: JWT, user?: User}) {
+      if (user) {
+        token.role = user.role || 'CLIENT'
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.role = token.role
+      }
+      return session
+    }
+  }
+} satisfies NextAuthConfig
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth(config)
